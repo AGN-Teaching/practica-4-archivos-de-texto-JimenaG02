920660ea3a06981d96e361b3693f0479f4373c47
Jimena Garcia <jimena.garcia@cua.uam.mx>
2025-04-16 15:34:04 -0600

    Update Correccion_textos.py

--- Correccion_textos.py
+++ Correccion_textos.py
@@ -1,19 +1,26 @@
+def comparar_palabras(nombre_archivo):
+    # Cargar palabras correctas
+    palabras_archivo = open("palabras.txt", 'r', encoding='utf-8') # encoding='utf-8 soporta todos los caracteres especiales del español
+    palabras_correctas = {palabra.strip().lower(): 0 for palabra in palabras_archivo}
+    palabras_archivo.close()
 
-
-def comparar_palabras(texto):
-
-    palabras_correctas = {open("palabras.txt", 'r')}
+    # Abrir archivo a revisar
+    archivo_usuario = open(nombre_archivo, 'r', encoding='utf-8')
+    texto = archivo_usuario.read().lower()
+    archivo_usuario.close()
 
     errores = 0
-    for caracter in texto:
-        caracter += caracter
-        if caracter.isspace() or caracter in ".!?,;:":  # Si hay un espacio o contiene esos caracteres los cuenta como palabras
-            print(caracter)
+    palabra_actual = ""
 
-            if caracter != palabras_correctas:
-                errores += 1
-            else:
-                caracter = ""
-                return
+    for caracter in texto:
+        if caracter.isalpha() or caracter == "'":
+            palabra_actual += caracter
+        else:
+            if palabra_actual:
+                #Verificar si la palabra esta en el diccionario
+                if palabra_actual not in palabras_correctas:
+                    errores += 1
+                    print("Palabra incorrecta: ", palabra_actual)
+                palabra_actual = ""
 
     return errores
